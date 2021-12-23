4f8483a5e730a8d3852fa09cdd9f6fe96a8a047c
Rodrigo Viñales <rodrigo11@live.com.ar>
2021-12-23 18:32:12 -0300

    Proyecto Final

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,15 +7,25 @@ import logoHeader from "../img/LOGOshopcomputers.png";
 import { useContext } from 'react';
 import { UserContext } from './Context/UserContext';
 import ModalLogin from './ModalLogin';
+import { useCollection } from '../hooks/useCollection';
 
 const NavComponent = () => {
 
   const { logged, logout } = useContext(UserContext)
+  const { productos: productos } = useCollection("productos")
+
+  const listadoCategorias = productos.reduce((acc, item) => {
+    if (!acc.includes(item.categoria)) {
+      acc.push(item.categoria);
+    }
+    return acc;
+  }, [])
 
   return (
 
     <header className="App-header">
       <Link to="/"><img src={logoHeader} className="logoHeader col-6" alt="logoHeader" /></Link>
+
       <Navbar expand="sm" collapseOnSelect>
         <Navbar.Brand>
           <img src={logoNav} className="logoNav" alt="logoNav" /> {' '}
@@ -24,16 +34,16 @@ const NavComponent = () => {
           <NavLink className="dropdown-item" to="/">INICIO</NavLink>
           <NavLink className="dropdown-item" to="/productos/">PRODUCTOS</NavLink>
           <NavDropdown title="CATEGORIAS" id="navbarScrollingDropdown">
-            <NavLink className="dropdown-item" to="/productos/MOUSE">MOUSE</NavLink>
-            <NavLink className="dropdown-item" to="/productos/TECLADOS">TECLADOS</NavLink>
-            <NavLink className="dropdown-item" to="/productos/ALMACENAMIENTO">ALMACENAMIENTO</NavLink>
-            <NavLink className="dropdown-item" to="/productos/ROUTER">ROUTER</NavLink>
-            <NavLink className="dropdown-item" to="/productos/PARLANTES">PARLANTES</NavLink>
+            {listadoCategorias.map(e => (
+              <div key={e}>
+                <NavLink className="dropdown-item" to={`/productos/${e}`}>{e}</NavLink>
+              </div>
+            ))}
             <NavDropdown.Divider />
-            <NavDropdown.Item>OFERTAS</NavDropdown.Item>
+            <NavDropdown.Item className='disabled'>OFERTAS - PROXIMAMENTE</NavDropdown.Item>
           </NavDropdown>
           <NavLink className="dropdown-item" to="/contacto">CONTACTO</NavLink>
-          <Link to="cart" style={{ textDecoration: 'none' }}><CartWidget /></Link>          
+          <Link to="cart" style={{ textDecoration: 'none' }}><CartWidget /></Link>
           {
             logged
               ?
@@ -41,27 +51,25 @@ const NavComponent = () => {
               :
               <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalLogin">LOGIN</button>
           }
-
         </Nav>
         {
           logged
-          ?
-          <>
-              <span style={{ 
-                color: "black", 
+            ?
+            <>
+              <span style={{
+                color: "black",
                 fontSize: 14,
-              }}>Habilitada la seccion ADMIN</span>
+              }}>Habilitada seccion ADMIN</span>
               <button type="button" className="btn btn-primary mx-3" onClick={logout}>Logout</button>
             </>
             :
-            <></>
-          }
+            <p style={{ color: "black", fontSize: 12, fontWeight: 700, fontFamily: "Montserrat", marginLeft: 5 }}>ACCESO A PERSONAL</p>
+        }
       </Navbar>
       <ModalLogin />
     </header>
   )
 }
-
 
 export default NavComponent;
 
